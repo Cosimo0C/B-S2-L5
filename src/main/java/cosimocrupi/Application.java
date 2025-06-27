cebcb49e8933ff4f6a2270b6acc3b438388b2ae6
Cosimo0C <cosimocrupi01@gmail.com>
2025-06-27 12:54:29 +0200

    test collezione

--- src/main/java/cosimocrupi/Application.java
+++ src/main/java/cosimocrupi/Application.java
@@ -1,5 +1,6 @@
 package cosimocrupi;
 
+import cosimocrupi.entita.Collezione;
 import cosimocrupi.entita.Genere;
 import cosimocrupi.entita.GiocoTavola;
 import cosimocrupi.entita.Videogioco;
@@ -11,6 +12,7 @@ public class Application {
         Videogioco v2= new Videogioco(113, "Uncharted", 2014, 55, "PS4", 150, Genere.AVVENTURA);
         GiocoTavola g1= new GiocoTavola(211, "Monopoli", 2000, 25, 6, 60);
         GiocoTavola g2= new GiocoTavola(212, "Uno", 2005, 15, 4, 60);
-
+        Collezione collezione=new Collezione();
+        collezione.addElemento();
     }
 }
--- src/main/java/cosimocrupi/entita/Collezione.java
+++ src/main/java/cosimocrupi/entita/Collezione.java
@@ -1,4 +1,24 @@
 package cosimocrupi.entita;
 
+import java.util.Scanner;
+
 public class Collezione {
+    public void addElemento(){
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Ciao! Benvenuto nel gestore dell'aggiunta di un gioco!");
+        int ris = 0;
+        do {
+            System.out.println("Che gioco vorresti aggiungere? Videogioco(1) o Gioco Da tavola(2). 0 per terminare.");
+            ris=Integer.parseInt(scanner.nextLine());
+            if (ris>=0 & ris<3){
+                System.out.println("ciao!");
+            } else if (ris==0) {
+                System.out.println("Grazie! Arrivederci!");
+            }else {
+                System.out.println("Il numero non è compreso tra le risposte possibili! Riprova");
+                addElemento();
+            }
+        }while(ris!=0);
+
+    }
 }
